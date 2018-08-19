9606d462af87eb7d139e4e9c20df77060a0c07b4
ms-examples <niblexis@gmail.com>
2018-08-19 09:00:30 -0700

    Added equipment section
    Covers equipment packages and tools, as well as weapon properties.
    
    May expand this section later and add armor/weapons. But it's unlikely.

---
+++ js/data_equipment.js
@@ -0,0 +1,396 @@
+data_equipment_packs = [
+     {
+         title: "Burglar's Pack",
+         icon: "robber",
+         subtitle: "Cost: 16gp",
+         description: "Cost: 16gp",
+         reference: "PHB, pg. 151",
+         bullets: [
+             "Includes a backpack, a bag of 1,000 ball bearings, 10 feet of string, a bell, 5 candles, a crowbar, a hammer, 10 pitons, a hooded lantern, 2 flasks of oil, 5 days rations, a tinderbox, and a waterskin. The pack also has 50 feet of hempen rope strapped to the side of it."
+         ]
+     },
+     {
+         title: "Diplomat's Pack",
+         icon: "shaking-hands",
+         subtitle: "Cost: 39gp",
+         description: "Cost: 39gp",
+         reference: "PHB, pg. 151",
+         bullets: [
+             "Includes a chest, 2 cases for maps and scrolls, a set of fine clothes, a bottle of ink, an ink pen, a lamp, 2 flasks of oil, 5 sheets of paper, a vial of perfume, sealing wax, and soap."
+         ]
+     },
+     {
+         title: "Dungeoneer's Pack",
+         icon: "dungeon-gate",
+         subtitle: "Cost: 12gp",
+         description: "Cost: 12gp",
+         reference: "PHB, pg. 151",
+         bullets: [
+             "Includes a backpack, a crowbar, a hammer, 10 pitons, 10 torches, a tinderbox, 10 days of rations, and a waterskin. The pack also has 50 feet of hempen rope strapped to the side of it."
+         ]
+     },
+     {
+         title: "Entertainer's Pack",
+         icon: "drama-masks",
+         subtitle: "Cost: 40gp",
+         description: "Cost: 40gp",
+         reference: "PHB, pg. 151",
+         bullets: [
+             "Includes a backpack, a bedroll, 2 costumes, 5 candles, 5 days of rations, a waterskin, and a disguise kit."
+         ]
+     },
+     {
+         title: "Explorer's Pack",
+         icon: "journey",
+         subtitle: "Cost: 10gp",
+         description: "Cost: 10gp",
+         reference: "PHB, pg. 151",
+         bullets: [
+             "Includes a backpack, a bedroll, a mess kit, a tinderbox, 10 torches, 10 days of rations, and a waterskin. The pack also has 50 feet of hempen rope strapped to the side of it."
+         ]
+     },
+     {
+         title: "Priest's Pack",
+         icon: "holy-symbol",
+         subtitle: "Cost: 19gp",
+         description: "Cost: 19gp",
+         reference: "PHB, pg. 151",
+         bullets: [
+             "Includes a backpack, a blanket, 10 candles, a tinderbox, an alms box, 2 blocks of incense, a censer, vestments, 2 days of rations, and a waterskin."
+         ]
+     },
+     {
+         title: "Scholar's Pack",
+         icon: "bookmark",
+         subtitle: "Cost: 40gp",
+         description: "Cost: 40gp",
+         reference: "PHB, pg. 151",
+         bullets: [
+             "Includes a backpack, a book of lore, a bottle of ink, an ink pen, 10 sheets of parchment, a little bag of sand, and a small knife."
+         ]
+     },
+]
+
+data_equipment_tools_norm = [
+     {
+         title: "Disguise Kit",
+         icon: "domino-mask",
+         subtitle: "Cost: 25gp",
+         description: "Cost: 25gp",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "This pouch of cosmetics, hair dye, and small props lets you create disguises that change your physical appearance. Proficiency with this kit lets you add your proficiency bonus to any ability checks you make to create a visual disguise."
+         ]
+     },
+     {
+         title: "Forgery Kit",
+         icon: "letter-bomb",
+         subtitle: "Cost: 15gp",
+         description: "Cost: 15gp",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "This small box contains a variety of papers and parchments, pens and inks, seals and sealing wax, gold and silver leaf, and other supplies necessary to create convincing forgeries of physical documents. Proficiency with this kit lets you add your proficiency bonus to any ability checks you make to create a physical forgery of a document."
+         ]
+     },
+     {
+         title: "Gaming Set",
+         icon: "perspective-dice-six-faces-random",
+         subtitle: "Cost: Varied",
+         description: "Cost: Varied",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "This item encompasses a wide range of gaming pieces, including dice and decks of cards (for games such as Three-Dragon Ante). A few common examples appear on the Tools table, but other kinds of gaming sets exist. If you are proficient with a gaming set, you can add your proficiency bonus to ability checks you make to play a game with that set. Each type of gaming set requires a seperate proficiency.",
+             "<b>Dice set:</b> 1sp <br /> <b>Dragonchess set:</b> 1gp <br /> <b>Playing card set:</b> 5sp <br /> <b>Three-Dragon Ante set:</b> 1gp <br />"
+         ]
+     },
+     {
+         title: "Herbalism Kit",
+         icon: "spoted-flower",
+         subtitle: "Cost: 5gp",
+         description: "Cost: 5gp",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "This kit contains a variety of instruments such as clippers, mortar and pestle, and pouches and vials used by herbalists to create remedies and potions. Proficiency witht his kit lets you add your proficiency bonus to any ability checks you make to identify or apply herbs. Also, proficiency with this kit is required to create antitoxin and <i>potions of healing.</i>"
+         ]
+     },
+     {
+         title: "Musical Instrument",
+         icon: "guitar",
+         subtitle: "Cost: Varied",
+         description: "Cost: Varied",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "Several of the most common types of musical instruments are shown on the table as examples. If you have proficiency with a given musical instrument, you can add your proficiency bonus to any ability checks you make to play music with the instrument. A bard can use a musical instrument as a spellcasting focus. Each type of musical instrument requires a seperate proficiency.",
+             "<b>Bagpipes:</b> 30gp <br /> <b>Drum:</b> 6gp <br /> <b>Dulcimer:</b> 25gp <br /> <b>Flute:</b> 2gp <br /> <b>Lute:</b> 35gp <br /> <b>Lyre:</b> 30gp <br /> <b>Horn:</b> 3gp <br /> <b>Pan flute:</b> 12gp <br /> <b>Shawm:</b> 2gp <br /> <b>Viol:</b> 30gp <br />"
+         ]
+     },
+     {
+         title: "Navigator's Tools",
+         icon: "compass",
+         subtitle: "Cost: 25gp",
+         description: "Cost: 25gp",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "This set of instruments is used for navigation at sea. Proficiency with navigator's tools lets you chart a ship's course and follow navigation charts. In addition, these tools allow you to add your proficiency bonus to any ability checks you make to avoid getting lost at sea."
+         ]
+     },
+     {
+         title: "Poisoner's Kit",
+         icon: "poison-bottle",
+         subtitle: "Cost: 50gp",
+         description: "Cost: 50gp",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "A poisoner's kit includes the vials, chemicals, and other equipment necessary for the creation of poisons. Proficiency with this kit lets you add your proficiency bonus to ability checks you make to craft or use poisons."
+         ]
+     },
+     {
+         title: "Thieves' Tools",
+         icon: "lockpicks",
+         subtitle: "Cost: 25gp",
+         description: "Cost: 25gp",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "This set of tools includes a small file, a set of lock picks, a small mirror mounted on a metal handle, a set of narrow-bladed scissors, and a pair of pliers. Proficiency with these tools lets you add your proficiency bonus to any ability checks you make to disarm traps or open locks."
+         ]
+     },
+]
+
+data_equipment_tools_artisan = [
+     {
+         title: "Artisan's Tools",
+         icon: "hammer-nails",
+         subtitle: "Tools used for a craft",
+         description: "Tools used for a craft",
+         reference: "PHB, pg. 154",
+         bullets: [
+             "These special tools include the items needed to pursue a craft or trade. The table shows examples of the most common types of tools, each providing items related to a single craft. Proficiency with a set of artisan’s tools lets you add your proficiency bonus to any ability checks you make using the tools in your craft. Each type of artisan’s tools requires a separate proficiency."
+         ]
+     },
+     {
+         title: "Alchemist's Supplies",
+         icon: "potion-ball",
+         subtitle: "Cost: 50gp",
+         description: "Cost: 50gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Brewer's Supplies",
+         icon: "beer-stein",
+         subtitle: "Cost: 20gp",
+         description: "Cost: 20gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Calligrapher's Supplies",
+         icon: "folded-paper",
+         subtitle: "Cost: 10gp",
+         description: "Cost: 10gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Carpenter's Tools",
+         icon: "hand-saw",
+         subtitle: "Cost: 8gp",
+         description: "Cost: 8gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Cobbler's Tools",
+         icon: "boots",
+         subtitle: "Cost: 5gp",
+         description: "Cost: 5gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Cook's Utensils",
+         icon: "meat-cleaver",
+         subtitle: "Cost: 1gp",
+         description: "Cost: 1gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Glassblower's Tools",
+         icon: "wine-glass",
+         subtitle: "Cost: 30gp",
+         description: "Cost: 30gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Jeweler's Tools",
+         icon: "engagement-ring",
+         subtitle: "Cost: 25gp",
+         description: "Cost: 25gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Leatherworker's Tools",
+         icon: "leather-vest",
+         subtitle: "Cost: 5gp",
+         description: "Cost: 5gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Mason's Tools",
+         icon: "tombstone",
+         subtitle: "Cost: 10gp",
+         description: "Cost: 10gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Painter's Supplies",
+         icon: "paint-brush",
+         subtitle: "Cost: 10gp",
+         description: "Cost: 10gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Potter's Tools",
+         icon: "flower-pot",
+         subtitle: "Cost: 10gp",
+         description: "Cost: 10gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Smith's Tools",
+         icon: "anvil-impact",
+         subtitle: "Cost: 20gp",
+         description: "Cost: 20gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Tinker's Tools",
+         icon: "tinker",
+         subtitle: "Cost: 50gp",
+         description: "Cost: 50gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Weaver's Tools",
+         icon: "wool",
+         subtitle: "Cost: 1gp",
+         description: "Cost: 1gp",
+         reference: "PHB, pg. 154",
+     },
+     {
+         title: "Woodcarver's Tools",
+         icon: "bone-knife",
+         subtitle: "Cost: 1gp",
+         description: "Cost: 1gp",
+         reference: "PHB, pg. 154",
+     },
+]
+
+data_equipment_weapons_properties = [
+     {
+         title: "Ammunition",
+         icon: "heavy-arrow",
+         subtitle: "Weapons that use ammunition",
+         description: "Weapons that use ammunition",
+         reference: "PHB, pgs. 146-147",
+         bullets: [
+             "You can use a weapon that has the ammunition property to make a ranged attack only if you have the ammunition to fire from the weapon. Each time you attack with the weapon, you expend one piece of ammunition. Drawing the ammunition from a quiver, case, or other container is part of the attack. At the end of the battle, you can recover half of your expended ammunition to search the battlefield.",
+             "If you use a weapon that has the ammunition property to make a melee attack, you treat the weapon as an improvised weapon. A sling must be loaded to deal any damage when used in this way."
+         ]
+     },
+     {
+         title: "Finesse",
+         icon: "shard-sword",
+         subtitle: "Weapons that can use DEX or STR",
+         description: "Weapons that can use DEX or STR",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "When making an attack with a finesse weapon, you use your choice of your Strength or Dexterity modifier for the attack and damage rolls. You must use the same modifier for both rolls."
+         ]
+     },
+     {
+         title: "Heavy",
+         icon: "anvil",
+         subtitle: "Weapons too heavy for small creatures",
+         description: "Weapons too heavy for small creatures",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "Small creatures have disadvantage on attack rolls with heavy weapons. A heavy weapon's size and bulk make it too large for a Small creature to use effectively."
+         ]
+     },
+     {
+         title: "Light",
+         icon: "feather",
+         subtitle: "Weapon that is easy to handle",
+         description: "Weapon that is easy to handle",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "A light weapon is small and easy to handle, making it ideal for use when fighting with two weapons."
+         ]
+     },
+     {
+         title: "Loading",
+         icon: "crossbow",
+         subtitle: "Weapon with long loading time",
+         description: "Weapon with long loading time",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "Because of the time required to load this weapon, you can fire only one piece of ammunition from it when you use an action, bonus action, or reaction to fire it, regardless of the number of attacks you can normally make."
+         ]
+     },
+     {
+         title: "Range",
+         icon: "lightning-bow",
+         subtitle: "A ranged weapon",
+         description: "A ranged weapon",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "A weapon that can be used to make a ranged attack has a range shown in parentheses after the ammunition or thrown property. The range lists two numbers. The first is the weapon's normal range in feet, and the second indicates the weapon's maximum range. When attacking a target beyond normal range, you have disadvantage on the attack roll. You can't attack a target beyond the weapon's long range."
+         ]
+     },
+     {
+         title: "Reach",
+         icon: "halberd",
+         subtitle: "A weapon with longer reach than normal",
+         description: "A weapon with longer reach than normal",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "This weapon adds 5 feet to your reach when you attack with it."
+         ]
+     },
+     {
+         title: "Thrown",
+         icon: "barbed-spear",
+         subtitle: "A weapon that can be thrown",
+         description: "A weapon that can be thrown",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "If a weapon has the thrown property, you can throw the weapon to make a ranged attack. If the weapon is a melee weapon, you use the same ability modifier for that attack roll and damage roll that you would use for a melee attack with the weapon. For example, if you throw a handaxe, you use your Strength, but if you throw a dagger, you can use either your Strength or your Dexterity, since the dagger has the finesse property."
+         ]
+     },
+     {
+         title: "Two-Handed",
+         icon: "axe-swing",
+         subtitle: "A weapon that uses both hands",
+         description: "A weapon that uses both hands",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "This weapon requires two hands to use"
+         ]
+     },
+     {
+         title: "Versatile",
+         icon: "relic-blade",
+         subtitle: "A weapon that can use one or two hands",
+         description: "A weapon that can use one or two hands",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "This weapon can be used with one or two hands. A damage value in parentheses appears with the property-- the damage weapon the weapon is used with two hands to make a melee attack."
+         ]
+     },
+     {
+         title: "Special",
+         icon: "perspective-dice-six-faces-random",
+         subtitle: "A special type of property",
+         description: "A special type of property",
+         reference: "PHB, pg. 147",
+         bullets: [
+             "A weapon with the special property has unusual rules governing its use, explained in the weapon's description."
+         ]
+     },
+]
