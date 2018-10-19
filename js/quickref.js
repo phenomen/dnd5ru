e2b3d02e66fc4625a527012228c160b7e8b35991
AVX <avx@xonr.ru>
2018-10-19 16:38:23 +0300

    Make modal-backdrop always full screen.

--- js/quickref.js
+++ js/quickref.js
@@ -34,7 +34,6 @@ function show_modal(data, color, type) {
 
     $("body").addClass("modal-open");
     $("#modal").addClass("modal-visible");
-    $("#modal-backdrop").css("height", window.innerHeight + "px");
     $("#modal-container").css("background-color", color).css("border-color", color);
     $("#modal-title").text(title).append("<span class=\"float-right\">" + type + "</span>");
     $("#modal-subtitle").text(subtitle);
